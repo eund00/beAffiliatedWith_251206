3fce986c0f0c5b119031f492e3100e29907c01dc
eundoo <leeeunj00@naver.com>
2025-12-05 23:30:19 +0900

    Add files via upload

---
+++ print.js
@@ -0,0 +1,45 @@
+function printOnlyArea() {
+  const userNote = prompt("닉네임을 입력하세요!", "");
+  const printContents = document.getElementById("printArea").innerHTML;
+  const printWindow = window.open("", "_blank");
+  const now = new Date();
+  const dateString = now.toLocaleString();
+
+  let html = `
+    <html>
+    <head>
+      <title>Print</title>
+      <style>
+        body { margin: 0; padding: 20px; }
+        .canvas { font-size: 13px; padding: 10px;width: 200px;border:1px solid #ccc}
+      </style>
+    </head>
+    <body>
+      <p style="font-size: 13px;padding-left: 20px">노골적으로 부속하며 ㅁ(미음)까지...</p>
+      <p style="font-style: italic;">========== result ==========<p>
+      <p><span>${userNote}</span>님의 뒤섞임....</p>
+      ${printContents}
+      <p style="font-size: 13px">
+        뒤섞인 
+        문장들에서 
+        힌트를 
+        찾아보아요☘️
+      </p>
+      <p style="font-style: italic;">===========================<p>
+      <p style="font-size: 13px">
+        ${dateString}
+      <p>
+    </body>
+    </html>
+  `;
+
+  printWindow.document.open();
+  printWindow.document.write(html);
+  printWindow.document.close();
+
+  setTimeout(() => {
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+  }, 150);
+}
