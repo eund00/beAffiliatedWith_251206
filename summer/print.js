aaa76f7df311b3e607d70d672933a9912875ef0f
eundoo <leeeunj00@naver.com>
2025-12-06 00:15:08 +0900

    Update print.js

--- summer/print.js
+++ summer/print.js
@@ -11,28 +11,25 @@ function printOnlyArea() {
       <title>Print</title>
       <style>
         body { margin: 0; padding: 20px; }
-        .canvas { font-size: 15px;padding: 10px;width: 220px;border:5px solid #000 }
-        .word-card { margin-right: 0.3rem; }
+        .canvas { font-size: 15px;padding: 10px;width: 220px;border:1px solid #ccc}
       </style>
     </head>
     <body>
-      <p style="font-size: 15px;">노골적으로 부속하며<br> ㅁ(미음)까지...</p>
-      <p>======= result =======<p>
+      <p style="padding-left: 15px">노골적으로 부속하며 ㅁ(미음)까지...</p>
+      <p style="font-style: italic;">======= result ========<p>
       <p><span>${userNote}</span>님의 뒤섞임....</p>
       ${printContents}
       <p>
-        뒤섞인 문장들에서 힌트를 찾아보아요☘️
+        뒤섞인 
+        문장들에서 
+        힌트를 
+        찾아보아요☘️
       </p>
       <p style="font-style: italic;">======================<p>
-      <p style="font-size: 13px">${dateString}</p>
-      <p style="padding-bottom: 20px;">end.</p>
-
-      <script>
-        window.onload = () => {
-          window.print();
-          window.close();
-        }
-      <\/script>
+      <p style="font-size: 13px">
+        ${dateString}
+      </p>
+      <p></p>
     </body>
     </html>
   `;
@@ -40,5 +37,10 @@ function printOnlyArea() {
   printWindow.document.open();
   printWindow.document.write(html);
   printWindow.document.close();
-}
 
+  setTimeout(() => {
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+  }, 150);
+}
